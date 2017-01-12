f969a946c025802c8b9d4baeceb8732016d32614
jooke42 <ialexandre.marteaux@gmail.com>
2017-01-12 12:48:49 +0100

    Update of .travis.yml
    Signed-off-by: jooke42 <ialexandre.marteaux@gmail.com>

--- api/models/Topic.py
+++ api/models/Topic.py
@@ -6,7 +6,7 @@ from api.models.Component import Component
 class Topic(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User)
-    components = models.ManyToManyField(Component, blank=True, null=True)
+    components = models.ManyToManyField(Component, blank=True)
     published_date = models.DateTimeField
     finished_date = models.DateTimeField
 
