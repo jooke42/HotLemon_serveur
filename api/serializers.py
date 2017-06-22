17407fde0ff234f1b58f992ffe4917964a68ec15
Alexandre Marteaux <alexandre.marteaux@outlook.fr>
2017-06-22 10:42:27 +0200

    modify serializers of topic

--- api/serializers.py
+++ api/serializers.py
@@ -19,7 +19,7 @@ class TopicSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Topic
-        fields = ('title', 'author', 'body', 'picture', 'vote_for', 'vote_against', 'longitude', 'latitude')
+        fields = ('id', 'title', 'author', 'body', 'picture', 'vote_for', 'vote_against', 'longitude', 'latitude')
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
